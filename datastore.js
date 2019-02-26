44c6e5cb14f2fd8da386e7a027c95d1f5c634bf6
Mehul Patel <mehulpatel118@gmail.com>
2019-02-26 11:11:49 -0500

    seprate datastore 2

---
+++ datastore.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const STORE = [
+  {
+    question: 'How many championships have the Lakers and Celtics won combined?',
+    answers: [
+      '13',
+      '15',
+      '25',
+      '33'
+    ],
+    correctAnswer: '33',
+    icon: 'https://i.imgur.com/Rue4wCj.png',
+    alt: 'tent icon'
+  },
+  {
+    question: 'Which league MVP has never made the NBA Finals?',
+    answers: [
+      'Derrick Rose',
+      'Russell Westbrook',
+      'James Harden',
+      'Kevin Durant'
+    ],
+    correctAnswer: 'Derrick Rose',
+    icon: 'https://i.imgur.com/iQ0FBhB.png',
+    alt: 'shield for 10 essentials icon'
+  },
+  {
+    question: 'Who famously is quoted as saying "Its just God disgused as Micahel Jordan"?',
+    answers: [
+      'Charles Barkely',
+      'Magic Johnson',
+      'Larry Bird',
+      'John Stockton'
+    ],
+    correctAnswer: 'Larry Bird',
+    icon: 'https://i.imgur.com/uXukVjk.png',
+    alt: 'weight icon'
+  },
+  {
+    question: 'Which of the following teams have been re-branded and moved to Okalahoma?',
+    answers: [
+      'Seattle Supersonics',
+      'Charlotte Hornets',
+      'Los Angeles Spark',
+      'New Jersey Nets'
+    ],
+    correctAnswer: 'Seattle Supersonics',
+    icon: 'https://i.imgur.com/i0Q515j.png',
+    alt: 'bear paw icon'
+  },
+  {
+    question: 'Who is he current leader in most three-point shots made in league history?',
+    answers: [
+      'Steph Curry',
+      'Ray Allen',
+      'Vince Carter',
+      'Jamal Crawford'
+    ],
+    correctAnswer: 'Ray Allen',
+    icon: 'https://i.imgur.com/LHRMcBd.png',
+    alt: 'hat icon'
+  }
+];
