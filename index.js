f5ae25b3a5aa9df1e7740bc482618730e9e8e3f3
Quonn Bernard <qber83@gmail.com>
2019-02-26 10:27:58 -0800

    added, start quiz button and quiz questions

--- index.js
+++ index.js
@@ -1,1 +1,82 @@
-'use strict';
+"use strict";
+let questionNumber = 0;
+
+//called when the "start quiz" button is clicked on the first screen
+function startQuiz() {
+  $("#startBtn").on("click", function() {
+    $(this).remove();
+    $("#questionBox").css("display", "block");
+  });
+}
+
+//generates the first question view
+function renderView() {
+  $("#questionBox").html(generateQuestion());
+  generateQuestion();
+}
+
+function generateQuestion() {
+  if (questionNumber < STORE.length) {
+    return `<div class="question-${questionNumber}">
+    <h2>${STORE[questionNumber].question}</h2>
+    <form>
+    <fieldset>
+    <label class="answerOption">
+    <input type="radio" value="${
+      STORE[questionNumber].answers[0]
+    }" name="answer" required>
+    <span>${STORE[questionNumber].answers[0]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${
+      STORE[questionNumber].answers[1]
+    }" name="answer" required>
+    <span>${STORE[questionNumber].answers[1]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${
+      STORE[questionNumber].answers[2]
+    }" name="answer" required>
+    <span>${STORE[questionNumber].answers[2]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${
+      STORE[questionNumber].answers[3]
+    }" name="answer" required>
+    <span>${STORE[questionNumber].answers[3]}</span>
+    </label>
+    <button type="submit" class="submitButton">Submit</button>
+    </fieldset>
+    </form>
+    </div>`;
+  } else {
+    renderResults();
+    restartQuiz();
+    $(".questionNumber").text(10);
+  }
+}
+
+function handleClickSubmitBtn() {
+  $("form").on("click", "button", function(event) {
+    event.preventDefault();
+    questionNumber++;
+    console.log(STORE.length, questionNumber);
+    renderView();
+  });
+}
+
+function renderResults() {
+  console.log("render results");
+}
+//Calls all other functions
+function handleQuiz() {
+  renderView();
+  startQuiz();
+  handleClickSubmitBtn();
+}
+
+function restartQuiz() {
+  console.log("restart quiz");
+}
+
+$(handleQuiz());
